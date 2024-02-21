70d63bbd408126a6bc17526b3a913e2850372207
Jianqiao Han <hanjianqiao@gmail.com>
2024-02-21 13:35:10 +0800

    add qt-easy-logger

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-
+#include <qt-easy-logger/logger.h>
 #include "samplewindow.h"
 #include "samplewindow2.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qInstallMessageHandler(h::Logger::messageHandler);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
@@ -19,9 +21,11 @@ int main(int argc, char *argv[])
         }
     }
 
+    qDebug() << "启动第一个例子";
     SampleWindow w;
     w.show();
 
+    qWarning() << "启动第二个例子";
     SampleWindow2 w2;
     w2.show();
 
