1da98b447aec0683f1dd7a52bfb9b3852464eed9
Jianqiao Han <hanjianqiao@gmail.com>
2024-02-25 20:42:01 +0800

    optimize code

--- framelesswindow.cpp
+++ framelesswindow.cpp
@@ -7,6 +7,7 @@
 #include <QHBoxLayout>
 #include <QCoreApplication>
 #include <QGuiApplication>
+#include <QTimer>
 
 #include "framelesswindow.h"
 
@@ -19,57 +20,73 @@
 #endif
 
 namespace h {
-
-FramelessWindowMaximizeButton::FramelessWindowMaximizeButton(QWidget *parent)
-    : QPushButton{parent}
-{}
-
-FramelessWindowMaximizeButton::FramelessWindowMaximizeButton(const QString &title, QWidget *parent)
-    : QPushButton{title, parent}
-{}
-
-bool FramelessWindowMaximizeButton::event(QEvent *e){
-    switch (e->type()) {
-    case QEvent::HoverMove:
-        setAttribute(Qt::WidgetAttribute::WA_UnderMouse);
-        mouseMoveEvent((QMouseEvent*)e);
-        break;
-    case QEvent::HoverLeave:
-        setAttribute(Qt::WidgetAttribute::WA_UnderMouse, false);
-        mouseMoveEvent((QMouseEvent*)e);
-        break;
-    default:
-        return QPushButton::event(e);
-    }
-    return true;
-}
-
-FramelessWindowTitleBarContentWidget::FramelessWindowTitleBarContentWidget(const QString &title, const QIcon &icon, QWidget *parent)
+FramelessWindowTitleBar::FramelessWindowTitleBar(QWidget *parent)
     : QWidget{parent}
 {
+}
+
+void FramelessWindowTitleBar::mousePressEvent(QMouseEvent *event){
+    window()->windowHandle()->startSystemMove();
+    QWidget::mousePressEvent(event);
+}
+
+void FramelessWindowTitleBar::mouseDoubleClickEvent(QMouseEvent *event){
+    if(window()->isMaximized()){
+        window()->showNormal();
+    }else{
+        window()->showMaximized();
+    }
+}
+
+bool FramelessWindowTitleBar::eventFilter(QObject *obj, QEvent *event){
+    if (!obj->isWidgetType()) return false; // standard check
+    if(obj == maximizeButton()){
+        switch (event->type()) {
+        case QEvent::HoverMove:
+            maximizeButton()->setAttribute(Qt::WidgetAttribute::WA_UnderMouse);
+            break;
+        case QEvent::HoverLeave:
+            maximizeButton()->setAttribute(Qt::WidgetAttribute::WA_UnderMouse, false);
+            break;
+        default:
+            break;
+        }
+    }
+    return false;
+}
+
+FramelessWindowDefaultTitleBar::FramelessWindowDefaultTitleBar(QWidget *parent)
+    : FramelessWindowTitleBar{parent}
+{
     m_iconLabel = new QLabel(this);
-    m_iconLabel->setPixmap(icon.pixmap(m_iconLabel->size()));
-    m_titleLabel = new QLabel(this);
-    m_titleLabel->setText(title);
+    m_titleLabel = new QLabel(qApp->applicationName(), this);
+    m_minimizeButton = new QPushButton(this);
+    m_maximizeButton = new QPushButton(this);
+    m_closeButton = new QPushButton(this);
+
+    // m_maximizeButton->setMouseTracking(true);
+
+    m_minimizeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarMinButton));
+    if(windowState() == Qt::WindowMaximized){
+        m_maximizeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarNormalButton));
+    }else{
+        m_maximizeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarMaxButton));
+    }
+    m_closeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarCloseButton));
+
+    /*
+     * BUG：FramelessWindow通过nativeEvent实现鼠标悬浮在最大化按钮触发snap layout的功能
+     * 时，发送QEvent::MouseMove事件，但是目标收到的是QEvent::HoverMove事件，无法触发
+     * QPushButton的状态变化。暂时通过这个代码处理事件改变QPushButton的状态。QPushButton
+     * 也可以是普通的QWidget
+    */
+    m_maximizeButton->installEventFilter(this);
 
     QHBoxLayout *rootLayout = new QHBoxLayout(this);
     rootLayout->setContentsMargins(8, 0, 0, 0);
     rootLayout->addWidget(m_iconLabel);
     rootLayout->addWidget(m_titleLabel);
-}
-
-FramelessWindowTitleBarControlWidget::FramelessWindowTitleBarControlWidget(QWidget *parent)
-    : QWidget{parent}
-{
-    m_minimizeButton = new QPushButton(this);
-    m_minimizeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarMinButton));
-    m_maximizeButton = new FramelessWindowMaximizeButton(this);
-    m_maximizeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarMaxButton));
-    m_closeButton = new QPushButton(this);
-    m_closeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarCloseButton));
-
-    QHBoxLayout *rootLayout = new QHBoxLayout(this);
-    rootLayout->setContentsMargins(0, 0, 0, 0);
+    rootLayout->addSpacerItem(new QSpacerItem(1, 1, QSizePolicy::Expanding, QSizePolicy::Preferred));
     rootLayout->addWidget(m_minimizeButton);
     rootLayout->addWidget(m_maximizeButton);
     rootLayout->addWidget(m_closeButton);
@@ -82,22 +99,7 @@ FramelessWindowTitleBarControlWidget::FramelessWindowTitleBarControlWidget(QWidg
     });
 }
 
-FramelessWindowMaximizeButton *FramelessWindowTitleBarControlWidget::maximizeButton() const{
-    return m_maximizeButton;
-}
-
-FramelessWindowTitleBar::FramelessWindowTitleBar(QWidget *contentWidget, QWidget *controlWidget, QPushButton *maximizeButton, QWidget *parent)
-    : QWidget{parent}, m_maximizeButton{maximizeButton}
-{
-    setMinimumHeight(20);
-    QHBoxLayout *rootLayout = new QHBoxLayout(this);
-    rootLayout->setContentsMargins(0, 0, 0, 0);
-    rootLayout->addWidget(contentWidget);
-    rootLayout->addSpacerItem(new QSpacerItem(1, 1, QSizePolicy::Expanding, QSizePolicy::Preferred));
-    rootLayout->addWidget(controlWidget);
-}
-
-void FramelessWindowTitleBar::onWindowStateChanged(Qt::WindowState state){
+void FramelessWindowDefaultTitleBar::onWindowStateChanged(Qt::WindowState state){
     switch(state){
     case Qt::WindowState::WindowNoState:
         m_maximizeButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TitleBarMaxButton));
@@ -110,25 +112,20 @@ void FramelessWindowTitleBar::onWindowStateChanged(Qt::WindowState state){
     }
 }
 
-QPushButton *FramelessWindowTitleBar::maximizeButton() const {
+QWidget *FramelessWindowDefaultTitleBar::maximizeButton() const{
     return m_maximizeButton;
 }
 
-void FramelessWindowTitleBar::mousePressEvent(QMouseEvent *event){
-    // QWidget::mousePressEvent(event);
-    window()->windowHandle()->startSystemMove();
+void FramelessWindowDefaultTitleBar::setTitle(const QString &title){
+    m_titleLabel->setText(title);
 }
 
-void FramelessWindowTitleBar::mouseDoubleClickEvent(QMouseEvent *event){
-    if(window()->isMaximized()){
-        window()->showNormal();
-    }else{
-        window()->showMaximized();
-    }
+void FramelessWindowDefaultTitleBar::setIcon(const QIcon &icon){
+    m_iconLabel->setPixmap(icon.pixmap(16, 16));
 }
 
 FramelessWindow::FramelessWindow(QWidget *parent)
-    : QWidget(parent), m_titleBar{nullptr}
+    : QWidget(parent), m_titleBar{nullptr}, m_bodyWidget{nullptr}
 {
     configFrameless();
 }
@@ -137,12 +134,19 @@ FramelessWindow::~FramelessWindow() {
 
 }
 
-void FramelessWindow::setTitleBar(FramelessWindowTitleBar *titleBar){
+void FramelessWindow::setTitleBarAndBodyWidget(FramelessWindowTitleBar *titleBar, QWidget *bodyWidget){
     m_titleBar = titleBar;
+    m_bodyWidget = bodyWidget;
+
+    QVBoxLayout *rootLayout = new QVBoxLayout(this);
+    rootLayout->setContentsMargins(0, 0, 0, 0);
+    m_bodyWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    rootLayout->addWidget(m_titleBar);
+    rootLayout->addWidget(m_bodyWidget);
 }
 
-void FramelessWindow::setMaximizeButton(QPushButton *button){
-    m_maximizeButton = button;
+FramelessWindowTitleBar *FramelessWindow::titleBar() const{
+    return m_titleBar;
 }
 
 void FramelessWindow::configFrameless() {
@@ -240,7 +244,7 @@ bool FramelessWindow::nativeEvent(const QByteArray &eventType, void *message, qi
             return true;
         }
 
-        if(m_maximizeButton){
+        if(m_titleBar && m_titleBar->maximizeButton()){
             //support highdpi/multiple screen
             QPoint gpos(ptMouse.x, ptMouse.y);
             QWindow * handle = window()->windowHandle();
@@ -250,25 +254,25 @@ bool FramelessWindow::nativeEvent(const QByteArray &eventType, void *message, qi
                 QPoint offset = screen->geometry().topLeft();
                 gpos = (gpos - offset) / screen->devicePixelRatio() + offset;
             }
-            QPoint lpos = m_maximizeButton->mapFromGlobal(gpos);
-            // TODO: send QEvent::MouseMove then will receive QEvent::HoverMove, don't know why
-            // more in MaximizeButton
-            if (m_maximizeButton->rect().contains(lpos)){
-                if(!m_maximizeButton->testAttribute(Qt::WA_UnderMouse)){
+            QPoint lpos = m_titleBar->maximizeButton()->mapFromGlobal(gpos);
+            // TODO: return true; 会让Qt获取不到事件，发送对应事件并更新，使maximizeButton响应鼠标移动
+            if (m_titleBar->maximizeButton()->rect().contains(lpos)){
+                if(!m_titleBar->maximizeButton()->testAttribute(Qt::WA_UnderMouse)){
                     QMouseEvent* evt = new QMouseEvent(
-                        QEvent::MouseMove,
+                        QEvent::HoverMove,
                         lpos,
                         gpos,
                         Qt::LeftButton,
                         QGuiApplication::mouseButtons(),
                         QGuiApplication::keyboardModifiers()
                         );
-                    QCoreApplication::postEvent(m_maximizeButton, evt);
+                    QCoreApplication::sendEvent(m_titleBar->maximizeButton(), evt);
+                    m_titleBar->maximizeButton()->update();
                 }
                 *result = HTMAXBUTTON;
                 return true;
             }else{
-                if(m_maximizeButton->testAttribute(Qt::WA_UnderMouse)){
+                if(m_titleBar->maximizeButton()->testAttribute(Qt::WA_UnderMouse)){
                     QMouseEvent* evt = new QMouseEvent(
                         QEvent::HoverLeave,
                         lpos,
@@ -277,7 +281,9 @@ bool FramelessWindow::nativeEvent(const QByteArray &eventType, void *message, qi
                         QGuiApplication::mouseButtons(),
                         QGuiApplication::keyboardModifiers()
                         );
-                    QCoreApplication::postEvent(m_maximizeButton, evt);
+                    QCoreApplication::sendEvent(m_titleBar->maximizeButton(), evt);
+                    m_titleBar->maximizeButton()->update();
+                    return true;
                 }
             }
         }
@@ -298,8 +304,10 @@ bool FramelessWindow::nativeEvent(const QByteArray &eventType, void *message, qi
         {
             *result = 0;
             if(isMaximized()){
+                qDebug() << "showNormal";
                 showNormal();
             }else{
+                qDebug() << "showMaximized";
                 showMaximized();
             }
             return true;
@@ -330,11 +338,14 @@ void FramelessWindow::changeEvent(QEvent *event){
     switch(event->type()){
     case QEvent::WindowStateChange:
         if(windowState() == Qt::WindowMaximized){
-            setContentsMargins(6, 6, 6, 6);
             m_titleBar->onWindowStateChanged(Qt::WindowState::WindowMaximized);
+            QTimer::singleShot(100, this, [this](){
+                int borderThickness = GetSystemMetrics(SM_CXSIZEFRAME);
+                setContentsMargins(borderThickness, borderThickness, borderThickness, borderThickness);
+            });
         }else if(windowState() == Qt::WindowState::WindowNoState){
-            setContentsMargins(0, 0, 0, 0);
             m_titleBar->onWindowStateChanged(Qt::WindowState::WindowNoState);
+            setContentsMargins(0, 0, 0, 0);
         }
         break;
     default:
--- framelesswindow.h
+++ framelesswindow.h
@@ -7,57 +7,40 @@
 
 namespace h {
 
-class FramelessWindowMaximizeButton : public QPushButton
-{
-    Q_OBJECT
-public:
-    FramelessWindowMaximizeButton(QWidget *parent = nullptr);
-    FramelessWindowMaximizeButton(const QString &title, QWidget *parent = nullptr);
-
-protected:
-    bool event(QEvent *e) override;
-
-};
-
-class FramelessWindowTitleBarContentWidget : public QWidget
-{
-    Q_OBJECT
-public:
-    FramelessWindowTitleBarContentWidget(const QString &title, const QIcon &icon, QWidget *parent);
-
-protected:
-    QLabel *m_iconLabel;
-    QLabel *m_titleLabel;
-};
-
-class FramelessWindowTitleBarControlWidget : public QWidget
-{
-    Q_OBJECT
-public:
-    FramelessWindowTitleBarControlWidget(QWidget *parent);
-    FramelessWindowMaximizeButton *maximizeButton() const;
-
-private:
-    QPushButton *m_minimizeButton;
-    FramelessWindowMaximizeButton *m_maximizeButton;
-    QPushButton *m_closeButton;
-};
-
 class FramelessWindowTitleBar : public QWidget
 {
     Q_OBJECT
 public:
-    FramelessWindowTitleBar(QWidget *contentWidget, QWidget *controlWidget, QPushButton *maximizeButton, QWidget *parent = nullptr);
-    void onWindowStateChanged(Qt::WindowState state);
-    QPushButton *maximizeButton() const;
+    FramelessWindowTitleBar(QWidget *parent);
+
+    virtual void onWindowStateChanged(Qt::WindowState state) = 0;
+    virtual QWidget *maximizeButton() const = 0;
+    virtual void setTitle(const QString &title) = 0;
+    virtual void setIcon(const QIcon &icon) = 0;
 
 protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseDoubleClickEvent(QMouseEvent *event) override;
+    bool eventFilter(QObject *object, QEvent *event) override;
+};
 
-protected:
+class FramelessWindowDefaultTitleBar : public FramelessWindowTitleBar
+{
+    Q_OBJECT
+public:
+    FramelessWindowDefaultTitleBar(QWidget *parent);
+
+    void onWindowStateChanged(Qt::WindowState state) override;
+    QWidget *maximizeButton() const override;
+    void setTitle(const QString &title) override;
+    void setIcon(const QIcon &icon) override;
+
+private:
+    QLabel *m_iconLabel;
+    QLabel *m_titleLabel;
+    QPushButton *m_minimizeButton;
     QPushButton *m_maximizeButton;
-
+    QPushButton *m_closeButton;
 };
 
 class FramelessWindow : public QWidget
@@ -68,8 +51,8 @@ public:
     FramelessWindow(QWidget *parent = nullptr);
     ~FramelessWindow();
 
-    void setTitleBar(FramelessWindowTitleBar *titleBar);
-    void setMaximizeButton(QPushButton *button);
+    void setTitleBarAndBodyWidget(FramelessWindowTitleBar *titleBar, QWidget *bodyWidget);
+    FramelessWindowTitleBar *titleBar() const;
 
 protected:
     bool nativeEvent(const QByteArray &eventType, void *message, qintptr *result) override;
@@ -83,8 +66,7 @@ protected:
 
 protected:
     FramelessWindowTitleBar *m_titleBar;
-    QPushButton *m_maximizeButton;
-
+    QWidget *m_bodyWidget;
 };
 }
 #endif // FRAMELESSWINDOW_H
